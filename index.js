055cbbd2728da57b9ce80fb610ba3ddd017d0401
Noah <noahdevelopsthings@gmail.com>
2022-11-05 22:48:21 -0300

    Posting working

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const { applicationKey, ipAddress } = require('./config');
+const { username, ipAddress } = require('./config');
 const PORT = 8080;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 app.use( express.json() );
 
+let last_request_time = null;
+const rate_limit = 15 * 1000;
 
 /*axios.get('https://discovery.meethue.com/')
     .then( response => {
@@ -20,46 +22,26 @@ app.use( express.json() );
 
 // Functions //
 
-function RGBtoXY(red, green, blue) {
-    red = red > 255 ? 255 : red;
-    green = green > 255 ? 255 : green;
-    blue = blue > 255 ? 255 : blue;
-
-    red = red < 0 ? 0 : red;
-    green = green < 0 ? 0 : green;
-    blue = blue < 0 ? 0 : blue;
-
-    R = red / 255.0;
-    G = green / 255.0;
-    B = blue / 255.0;
-
-    return R, G, B;
-}
-   
-
-
-axios.get(`https://${ipAddress}/clip/v2/resource/device`, {
-        rejectUnauthorized: false,
-        headers: {
-            "hue-application-key": applicationKey
-        },
+axios.get(`https://${ipAddress}/api/${username}/lights/`)
+    .then( response => {
+        for (const [key, value] of Object.entries(response.data)) {
+            lights.set(key, key)
+        }
     })
-        .then( response => {
-            const data = response.data.data;
-            for (let i = 1; i < data.length; i++) {
-                lights.set(data[i].metadata.name, data[i].id);
-            }
-        })
-        .catch( error => res.send(error) );
 
 const lights = new Map
 
-app.post('/:R/:G/:B', (req, res) => {
+app.post('/:H/:S/:B', (req, res) => {
+    if (last_request_time && Date.now() - last_request_time < rate_limit) {
+        res.send('Server is rate limited');
+        return;
+    }
+    last_request_time = Date.now();
     if (!ipAddress) {
         res.send('Bridge was not found, server is rate limited');
         return;
     }
-    let { R, G, B } = req.params;
+    let { H, S, B } = req.params;
     let identifier = req.body.identifier;
 
     let lightId = lights.get(identifier)
@@ -67,31 +49,46 @@ app.post('/:R/:G/:B', (req, res) => {
         let firstLight = lights.values().next().value;
         lightId = firstLight;
     }
-    let X, Y, Z = RGBtoXY(R, G, B);
 
-    axios.put(`https://${ipAddress}/clip/v2/resource/light/${lightId}`, {
-        rejectUnauthorized: false,
-        headers: {
-            "hue-application-key": applicationKey
-        },
-        body: {
-            "on": {
-                "on": true
-            },
-            "dimming": {
-                "brightness": Z
-            },
-            "color": {
-                "xy": {
-                    "x": X,
-                    "y": Y
-                }
-            }
-        }
-    })
-        .then( response => res.send("Success") )
+
+    let on = true;
+
+    console.log(typeof H, typeof S, typeof B);
+    axios.get(`https://${ipAddress}/api/${username}/lights/`)
+    axios.put(`https://${ipAddress}/api/${username}/lights/${lightId}/state/${lightId}`, {
+        on,
+        ... (H && { hue: parseInt(H) }),
+        ... (S && { sat: parseInt(S) }),
+        ... (B && { bri: parseInt(B) }),
+
+    }) 
+        .then( response => res.send(response) )
         .catch( error => res.send(error) );
-    
-    
-    console.log(lights[0]);
+})
+
+app.post('/off', (req, res) => {
+    if (last_request_time && Date.now() - last_request_time < rate_limit) {
+        res.send('Server is rate limited');
+        return;
+    }
+    last_request_time = Date.now();
+    if (!ipAddress) {
+        res.send('Bridge was not found, server is rate limited');
+        return;
+    }
+    let identifier = req.body.identifier;
+
+    let lightId = lights.get(identifier)
+    if (!lightId) {
+        let firstLight = lights.values().next().value;
+        lightId = firstLight;
+    }
+
+    let on = false;
+
+    axios.put(`https://${ipAddress}/api/${username}/lights/${lightId}/state/${lightId}`, {
+        on,
+    }) 
+        .then( response => res.send(response) )
+        .catch( error => res.send(error) );
 })
